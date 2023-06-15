a69c03eb9ed27073f1a54820b5e55cfea275648b
getachewm <getachew.mekonen12@gmail.com>
2023-06-14 19:47:45 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/11-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import os
+import sys
+
+Student = __import__('11-student').Student
+read_file = __import__('0-read_file').read_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+path = sys.argv[1]
+
+if os.path.exists(path):
+    os.remove(path)
