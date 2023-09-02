ed68ba83a00c9478ad2a0b67a581c55833750b4d
root <getachew.mekonen12@gmail.com>
2023-09-01 18:02:10 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" A script that fetches https://alx-intranet.hbtn.io/status and Uses urlib package. """
+""" A script that
+- Fetches https://alx-intranet.hbtn.io/status.
+- Uses urlib package.
+"""
+
 
 if __name__ == '__main__':
     import urllib.request
@@ -10,3 +14,4 @@ if __name__ == '__main__':
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
