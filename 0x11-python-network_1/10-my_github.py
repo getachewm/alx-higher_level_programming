abdc5c46753ae1bf94a3d62fdbe21dcc7a54844c
root <getachew.mekonen12@gmail.com>
2023-09-01 19:13:24 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-""" A script that takes your GitHub creds (username and password) uses the GitHub API to display your id """
-import sys
-import requests
-from requests.auth import HTTPBasicAuth
-
+"""
+a python script that takes GitHub credentials
+(username and password) and uses the GitHub API to display user's id
+"""
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    import requests
+    import sys
+
+    url = "https://api.github.com/user"
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    data_res = requests.get(url, auth=(user, passwd))
+
+    try:
+        data_json = data_res.json()
+        print(data_json["id"])
+    except Exception:
+        print("None")
