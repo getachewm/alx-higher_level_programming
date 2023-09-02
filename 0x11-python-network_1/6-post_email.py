cbb057238fe6323e9a60bbb8cf4933def65f4d0e
root <getachew.mekonen12@gmail.com>
2023-09-01 19:02:42 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" A  script that takes in a URL and an email address sends a POST req to the passed URL with the email as a para displays the body of the response."""
-
-import sys
-import requests
+"""A script that:
+- takes in a URL,
+- sends a req to the URL
+- displays the body of the response (decoded in utf-8).
+"""
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    import sys
+    from urllib import request, error
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
