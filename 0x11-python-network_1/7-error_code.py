6a077ceb2a46dd96214a2cda2c6a8977ee085007
root <getachew.mekonen12@gmail.com>
2023-09-01 19:09:24 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" A script that takes in a URL sends a req to the URL displays the body of the response."""
+"""
+a python script that sends a request to
+the URL passed in and displays the body of the response.
+"""
+
 import sys
 import requests
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
-
     r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
