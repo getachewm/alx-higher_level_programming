270f3eac8a920e1f5b5a88d08e8dc3a01a313653
root <getachew.mekonen12@gmail.com>
2023-09-04 19:04:01 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
-"""
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8")
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE '%%{userinput}%%' \
-ORDER BY id ASC".format(
-            userinput=sys.argv[4]))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
