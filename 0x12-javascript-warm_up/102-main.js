715f1e4cb62fbb2c102ad6f44f21f385b6cf3379
getachewm <122815980+getachewm@users.noreply.github.com>
2023-12-13 01:02:03 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/102-main.js
+++
@@ -1,4 +0,0 @@
-t addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
-addMeMaybe(4, function (nb) {
-  console.log('New value: ' + nb);
-});
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-/* script that prints 3 lines
-but by using an array of string and a loop */
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
-}
