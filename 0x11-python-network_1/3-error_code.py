3c383c7a9d9fe4fc2552b1254cde17528228aeaa
root <getachew.mekonen12@gmail.com>
2024-01-09 04:40:36 -0800

    0x11. Python - Network #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,18 +8,20 @@ Uses only the packages urllib and sys.
 import sys
 from urllib import request, error
 
+def fetch_url(url):
+    try:
+        with request.urlopen(url) as response:
+            return response.read().decode('utf-8')
+
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+        sys.exit(1)
+
 if __name__ == "__main__":
-    # Check if a URL is provided as a command-line argument
     if len(sys.argv) != 2:
         print("Usage: python script.py <URL>")
         sys.exit(1)
 
     url = sys.argv[1]
-
-    try:
-        with request.urlopen(url) as response:
-            content = response.read().decode('utf-8')
-            print(content)
-
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    content = fetch_url(url)
+    print(content)
