579bca0dc77f578f02fd61fa95894bb844bd0c13
root <getachew.mekonen12@gmail.com>
2024-01-11 03:29:26 -0800

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL,
-and displays the body of the response (decoded in utf-8).
-Handles urllib.error.HTTPError exceptions and prints the HTTP status code.
+takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 """
 
-from urllib import request, error
 import sys
-
-def fetch_url_content(url):
-    try:
-        with request.urlopen(url) as response:
-            return response.read().decode('utf-8')
-    except error.HTTPError as e:
-        print("Error code:", e.code)
-        return None
+from urllib import request, error
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
-    content = fetch_url_content(url)
 
-    if content is not None:
-        print(content)
+    own_request = request.Request(url)
+    try:
+        with request.urlopen(own_request) as response:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
