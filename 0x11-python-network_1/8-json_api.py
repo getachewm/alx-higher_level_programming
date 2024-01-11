47061a3a350f2ea59550946a8de065380b875306
root <getachew.mekonen12@gmail.com>
2024-01-11 04:45:48 -0800

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,7 +2,6 @@
 """
 Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
-"""
 
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
