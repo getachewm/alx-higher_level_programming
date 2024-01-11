c7eae4f9c4920cc30dae02ea1ae20c7a6e2a7546
root <getachew.mekonen12@gmail.com>
2024-01-11 03:18:31 -0800

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-Takes in a URL, sends a request to the URL, and displays the body of the response (decoded in utf-8).
-Handles urllib.error.HTTPError exceptions and prints: Error code: followed by the HTTP status code.
-Uses only the packages urllib and sys.
-"""
+"""Sends a POST request to a given URL with a given email.
 
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import sys
-from urllib import request, error
+import urllib.parse
+import urllib.request
 
-def fetch_url(url):
-    try:
-        with request.urlopen(url) as response:
-            return response.read().decode('utf-8')
-
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-        sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    content = fetch_url(url)
-    print(content)
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
